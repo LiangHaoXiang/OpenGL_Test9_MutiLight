d2591eee23c79a88142fc366fdb1c7e0328df4cd
LiangHaoXiang <1013568397@qq.com>
2021-07-19 15:15:16 +0800

    Initial Commit

---
+++ OpenGL_Test9_MutiLight/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL_Test9_MutiLight
+//
+//  Created by Haoxiang Liang on 2021/7/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
